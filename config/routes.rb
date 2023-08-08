c15cb0f8c5a9d1d0f43f33c7620ab509ad2aa3a4
Jawad.Nazari <Jawad.Nazari@outlook.com>
2023-08-08 16:12:25 +0500

    Add custome controller for registration

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-   devise_for :users
+   root to: 'recipes#index'
+ devise_for :users, controllers: {
+  registrations: 'users/registrations'
+}
    resources :recipes, only: [:index] do
   end
 end
