ac35bd3982898749ffdaf4d20391224ba34032b3
Jawad.Nazari <Jawad.Nazari@outlook.com>
2023-08-13 00:48:56 +0500

    Fixing test error

--- Gemfile
+++ Gemfile
@@ -11,7 +11,7 @@ gem 'sprockets-rails'
 
 # authentication
 gem 'devise'
-
+gem 'factory_bot_rails', '~> 6.2'
 # Use the Puma web server [https://github.com/puma/puma]
 gem 'puma', '~> 5.0'
 
--- spec/requests/recipes_spec.rb
+++ spec/requests/recipes_spec.rb
@@ -2,8 +2,13 @@ require 'rails_helper'
 
 RSpec.describe 'Recipes', type: :request do
   describe 'GET /index' do
-    it 'returns http success' do
-      get '/recipes/index'
+    it 'returns http success when authenticated' do
+      user = FactoryBot.create(:user)
+
+      post user_session_path, params: { user: { email: user.email, password: 'password' } }
+
+      get recipes_path
+
       expect(response).to have_http_status(:success)
     end
   end
