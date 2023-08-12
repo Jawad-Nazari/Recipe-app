d8a46364114316c322c449ec468c481f2f6b3bd8
Jawad.Nazari <Jawad.Nazari@outlook.com>
2023-08-12 05:18:18 +0500

    Add test for food and shopping list

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ApplicationRecord
   end
 
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :confirmable
+         :recoverable, :rememberable, :validatable
   has_many :foods, class_name: 'Food', foreign_key: 'user_id'
   has_many :recipes, class_name: 'Recipe', foreign_key: 'user_id'
 
